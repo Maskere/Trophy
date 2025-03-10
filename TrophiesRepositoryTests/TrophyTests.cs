2dbc0da48c8cb4339d7768c559a62125f29fb07a
Maskere <Mstachkruse@gmail.com>
2025-03-10 18:27:40 +0100

    Renaming again

---
+++ TrophiesRepositoryTests/TrophyTests.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassLibraryTrophy.Tests {
+    [TestClass()]
+    public class TrophyTests {
+        Trophy? trophy;
+
+        [TestInitialize]
+        public void Initializer(){
+            trophy = new("FastCoding", 2024);
+        }
+
+        [TestMethod()]
+        public void TestTrophyCompetionLengthLowerThanThreeException() {
+            string? exceptionTest = "Ab";
+            Assert.ThrowsException<ArgumentException>(() => new Trophy(exceptionTest,2024));
+        }
+
+        [TestMethod()]
+        public void TestTrophyCompetitionNullException() {
+            Assert.ThrowsException<ArgumentNullException>(() => new Trophy(null,2024));
+        }
+
+        [TestMethod()]
+        public void TestTrophyYearLowerThan1970Exception() {
+            Assert.ThrowsException<ArgumentException>(() => new Trophy("FastCoding",1969));
+        }
+
+        [TestMethod()]
+        public void TestTrophyYearHigherThan2025Exception() {
+            Assert.ThrowsException<ArgumentException>(() => new Trophy("FastCoding",2026));
+        }
+
+        [TestMethod()]
+        public void TropyhyToStringTest() {
+            string? shouldBe = "Trophy 0\nCompetition FastCoding\nYear 2024";
+            Assert.AreEqual(shouldBe, trophy?.ToString());
+        }
+    }
+}
